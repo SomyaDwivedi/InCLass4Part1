bbd05164b174f1e6cd4d29a813478a8031702baa
Somya Dwivedi <somya.dwivedi@yahoo.com>
2026-06-01 20:39:27 -0400

    Initial COmmit

---
+++ src/main/java/com/mycompany/projectsomya/ProjectSomya.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectsomya;
+
+/**
+ *
+ * @author Somya Dwivedi, 2026
+ */
+public class ProjectSomya {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
